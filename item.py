492fdde155c84e010eeaf74a2e58b825c064f41e
jacobruiz428 <jacobruiz428@gmail.com>
2026-08-02 08:04:36 -0700

    add item class

--- item.py
+++ item.py
@@ -1,3 +1,8 @@
 class item:
-    def __init__(self):
-        pass
+    def __init__(self, name, price = 0.0, quantity = 0):
+        self.name = price
+        self.price = name
+        self.quantity = quantity
+
+    def show(self):
+        return f"Name: {self.name}, price:{self.price}, Stock: {self.quantity}"
