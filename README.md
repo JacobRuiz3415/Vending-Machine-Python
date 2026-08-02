cb2e48eb5f939c3a8ca56102a2e1e86dc1a3e14a
jacobruiz428 <jacobruiz428@gmail.com>
2026-08-02 08:08:17 -0700

    added some more

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Vending Machine
 I have made a vending Machine program in Python
 Planned features
-- menu
-- restock
+- menu for options
+- restock  and sold function
 - stock file that holds the inventory
 - item class
--- item.py
+++ item.py
@@ -1,8 +1,14 @@
 class item:
     def __init__(self, name, price = 0.0, quantity = 0):
-        self.name = price
-        self.price = name
+        self.name = name
+        self.price = price
         self.quantity = quantity
 
     def show(self):
         return f"Name: {self.name}, price:{self.price}, Stock: {self.quantity}"
+
+    def restock(self, q):
+        self.quantity += q
+
+    def sold(self, s = 1):
+        self.quantity -= s
