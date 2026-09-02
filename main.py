f6291b95349991b5f12d00d04053d972b02b0293
jacobruiz428 <jacobruiz428@gmail.com>
2026-09-01 19:06:16 -0700

    testing file output

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#main program
-import item
+#Main Program
+from item import item
 
 print("Hello User")
 
@@ -10,3 +10,11 @@ I1 = item.item("soda", 3.44, 4)
 print(I1.show())
 I1.sold()
 print(I1.show())
+
+#testing output to a file
+f = open("history", "w")
+f.write(I1.show())
+
+#read the file
+f = open("history")
+print(f.read())
